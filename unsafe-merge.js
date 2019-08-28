34201f6515cf65fe6821c461130dfa5d1baf51ff
Sergey Pronin <20225829+sergey-pronin@users.noreply.github.com>
2019-08-28 11:39:25 +0200

    Create unsafe-merge.js

---
+++ unsafe-merge.js
@@ -0,0 +1,10 @@
+var merge = function(target, source) {
+    for(var attr in source) {
+        if(typeof(target[attr]) === "object" && typeof(source[attr]) === "object") {
+            merge(target[attr], source[attr]);
+        } else {
+            target[attr] = source[attr];
+        }
+    }
+    return target;
+};
